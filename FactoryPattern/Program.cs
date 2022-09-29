cfdcb81f859e8377321e89e57ee9fbd5b7b67302
JulianRenaud <midway6220@gmail.com>
2022-09-29 12:22:11 -0500

    First Commit

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,7 @@
+﻿using FactoryPattern;
+
+Console.WriteLine("How many wheels would you like on your vehicle?");
+var userInput = Console.ReadLine();
+VehicleFactory.GetVehicle(userInput);
+
+
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    internal class VehicleFactory
+    {
+        public static IVehicle GetVehicle(string numOfWheels)
+        {
+            switch (numOfWheels.ToLower())
+            {
+                case "two":
+                    var v1 = new Car();
+                    v1.Drive();
+                    return new Motorcycle();
+                case "2":
+                    var v2 = new Motorcycle();
+                    v2.Drive();
+                    return new Motorcycle();
+                case "four":
+                    var v3 = new Car();
+                    v3.Drive();
+                    return new Car();
+                case "4":
+                    var v4 = new Car();
+                    v4.Drive();
+                    return new Car();
+                default:
+                    Console.WriteLine("Sorry, we do not have a vehicle with that specific ammount of wheels in stock today!");
+                    return null;
+            }
+
+        }
+    }
+}
